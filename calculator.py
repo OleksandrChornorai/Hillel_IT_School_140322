c3fd21b0caee67b07077dc32d73c39430a52b630
AleksandrCh <acm.twx@gmail.com>
2022-03-27 15:05:19 +0300

    upd

--- calculator.py
+++ calculator.py
@@ -3,21 +3,22 @@ value_1 = float(input("Введи первое число:"))
 value_2 = float(input("Введи второе число:"))
 try:
     if input_case == '1':
-        result = value_1 + value_2
+        result = (value_1 + value_2)
+        print(f"= {result}")
     elif input_case == '2':
-        result = value_1 - value_2
+        result = (value_1 - value_2)
+        print(f"= {result}")
     elif input_case == '3':
-        result = value_1 * value_2
+        result = (value_1 * value_2)
+        print(f"= {result}")
     elif input_case == '4':
         try:
-            result = value_1 /value_2
+            result = (value_1 / value_2)
+            print(f"= {result}")
         except ZeroDivisionError:
             print("На 0 делить нельзя!!!")
     else:
         print("Неверная операция")
-        print(result)
-    print(f"= {result}")
 except ValueError:
     print("Не корректное значение")
     pass
-
