26bb7ec102b254ec316de3029b3dc3347bbd8570
AleksandrCh <acm.twx@gmail.com>
2022-03-27 17:44:25 +0300

    upd

--- calculator.py
+++ calculator.py
@@ -1,28 +1,29 @@
+print("\n>>> Calculator by Python ver. 1.0 <<<\n>>> Welcome! <<<\n")
 input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /\n")
 try:
     if input_case == '1':
         value_1 = float(input("Введи первое число:"))
         value_2 = float(input("Введи второе число:"))
         result = (value_1 + value_2)
-        print(f"= {result}")
+        print(f"Результат: {result} \n\n>>> Операция завершена <<<")
     elif input_case == '2':
         value_1 = float(input("Введи первое число:"))
         value_2 = float(input("Введи второе число:"))
         result = (value_1 - value_2)
-        print(f"= {result}")
+        print(f"Результат: {result} \n\n>>> Операция завершена <<<")
     elif input_case == '3':
         value_1 = float(input("Введи первое число:"))
         value_2 = float(input("Введи второе число:"))
         result = (value_1 * value_2)
-        print(f"= {result}")
+        print(f"Результат: {result} \n\n>>> Операция завершена <<<")
     elif input_case == '4':
         value_1 = float(input("Введи первое число:"))
         value_2 = float(input("Введи второе число:"))
         result = (value_1 / value_2)
-        print(f"= {result}")
+        print(f"Результат: {result} \n\n>>> Операция завершена <<<")
     else:
-        print("Неверная операция")
+        print("\n>>> Неверная операция! <<<")
 except ZeroDivisionError:
-    print("На 0 делить нельзя!!!")
+    print("\n>>> На 0 делить нельзя!!! <<<")
 except ValueError:
-    print("Не корректное значение")
+    print("\n>>> Не корректное значение <<<")
