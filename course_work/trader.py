a39f057086435a8f7ab779f96045fb881b4035cf
AleksandrCh <acm.twx@gmail.com>
2022-05-12 23:19:05 +0300

    upd

--- course_work/trader.py
+++ course_work/trader.py
@@ -8,12 +8,13 @@ class MoneyExchange:
         self.config_file = config_file
         self.exchanger = exchange_list
         self.read_config()
+        self.start_exchange()
         self.exchanger_list()
         self.course = self.exchanger_list()["course"]
         self.uah_acc = self.exchanger_list()["uah_acc"]
         self.usd_acc = self.exchanger_list()["usd_acc"]
         self.delta = self.exchanger_list()["delta"]
-        self.start_exchange()
+        self.change_course()
 
     def read_config(self):
         with open(self.config_file, 'r') as file:
@@ -31,21 +32,40 @@ class MoneyExchange:
 
     def change_course(self):
         new_course = round(random.uniform(self.course - self.delta, self.course + self.delta), 2)
-        return new_course
+        with open(self.exchanger, 'r') as file:
+            data = json.load(file)
+            data["course"] = new_course
+        with open(self.exchanger, 'w') as file:
+            json.dump(data, file)
 
 
 args = ArgumentParser()
-args.add_argument("operation", type=str)
-args.add_argument("value", type=float, nargs='?', default='')
-# args = args.parse_args()
-
+args.add_argument("operation", type=str, nargs='?', default='')
+args.add_argument("value", type=float, nargs='?', default='0')
+args = vars(args.parse_args())
+# print(f'{args["operation"]},{args["value"]}')
 config_file = '../Hillel_IT_School_140322/course_work/config.json'
 exchange_list = '../Hillel_IT_School_140322/course_work/exchange_list.json'
 make_exchange = MoneyExchange(config_file, exchange_list)
 result = make_exchange.read_config()
 result2 = make_exchange.exchanger_list()
-result3 = make_exchange.change_course()
-print(result, result2, result3)
+course = make_exchange.exchanger_list()["course"]
+uah_acc = make_exchange.exchanger_list()["uah_acc"]
+usd_acc = make_exchange.exchanger_list()["usd_acc"]
+try:
+    moneymaker = MoneyExchange(config_file, exchange_list)
+    if args["operation"] == 'RATE':
+        print(f'Курс:{course}')
+    elif args["operation"] == 'AVAILABLE':
+        print(f'UAH:{uah_acc}')
+        print(f'USD:{usd_acc}')
+    elif args["operation"] == 'NEXT':
+        make_exchange.change_course()
+    else:
+        pass
+except ValueError:
+    print('ERROR')
+# print(result, result2)
 # RATE - получение текущего курса (USD/UAH)
 # AVAILABLE - получение остатков по счетам
 # BUY XXX - покупка xxx долларов. При отсутсвии грвен для покупки выводит сообщение типа UNAVAILABLE, REQUIRED BALANCE UAH 2593.00, AVAILABLE 1000.00
